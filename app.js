cf15d3892c32035836bab2f3052b6af9a17cf8a0
gloomydumber <chromacrow@naver.com>
2021-01-21 23:30:39 +0900

    push roh dah

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
@@ -17,8 +19,8 @@ app.use(morgan("dev"));
 
 // app.use('/user', userRouter); // router 자체를 이용 => app.use
 
-app.use('/', globalRouter);
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
